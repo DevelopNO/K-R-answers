bc8f06521af3a0476bdca943f620cdda3aa73236
DevelopNO <noamohana@Gmail.com>
2018-10-03 23:47:09 +0300

    Exercise 1-5
    Reversing the order of the temprature to be from 300 to 0

---
+++ Exercise1_5.c
@@ -0,0 +1,33 @@
+/* Reverse order */
+/* I didn't use features that weren't learned untill this point (e.g. += etc.) */
+/* This is the original program 
+
+#include <stdio.h>
+
+int main()
+{
+  int fahr;
+  
+  for(fahr = 0; fahr <= 300; fahr = fahr + 20)
+  {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+  }
+  
+  return 0;
+}
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int fahr;
+  
+  for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+  {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+  }
+  
+  return 0;
+}
